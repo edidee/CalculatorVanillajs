58271dde00822882d28d13b2194be7fc75f1ff00
edidee <etokedidionge@gmail.com>
2020-11-05 09:14:44 +0100

    js update

--- main.js
+++ main.js
@@ -4,31 +4,99 @@ const calculator = {
   displayValue : '0',
   firstOperand : null,
   operator: null,
-  waitingForSecondOPerand: false
-}
+  waitingForSecondOperand: false
+};
 
+// display value
 function inputDigit(digit) {
-  const { displayValue } = calculator;
+  const {  displayValue , waitingForSecondOperand } = calculator;
 
-  calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  }
+  console.log(calculator);
 }
 
 
+
+
+
+// handle decimal
 function inputDecimal(dot)  {
-  
+  if (calculator.waitingForSecondOperand === true) {
+    calculator.displayValue = '0.'
+    calculator.waitingForSecondOperand = false;
+    return;
+  }
+
   if (!calculator.displayValue.includes(dot)){
     calculator.displayValue += dot;
   }
 }
 
 
-function updateValue(){
-  
+// handle operator
+function handleOperator(nextOperator) {
+  const { firstOperand, displayValue, operator } = calculator
+  const inputValue = parseFloat(displayValue);
+
+  if (operator && calculator.waitingForSecondOperand) {
+    calculator.operator = nextOperator;
+    console.log(calculator);
+    return;
+  }
+
+  if (firstOperand === null && !isNaN(inputValue)) {
+
+    calculator.firstOperand = inputValue;
+
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator);
+
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+  console.log(calculator);
+}
+
+// update display
+function updateValue(){ 
   output.value = calculator.displayValue
 }
 updateValue();
 
+// calculate
+function calculate(firstOperand, secondOperand, operator) {
 
+  if (operator === '+') {
+    return firstOperand + secondOperand;
+  } else if (operator === '-'){
+    return firstOperand - secondOperand;
+  } else if(operator === '*'){
+    return firstOperand * secondOperand;
+  } else if (operator === '/'){
+    return firstOperand / secondOperand;
+  }
+
+  return secondOperand;
+}
+
+// Reset
+function resetCalculator() {
+  calculator.displayValue = '0';
+  calculator.firstOperand = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+  console.log(calculator);
+}
+
+// Handle key press
 const keys = document.querySelectorAll('.first');
 
 for(let i = 0; i < keys.length; i++){
@@ -36,24 +104,28 @@ for(let i = 0; i < keys.length; i++){
   keys[i].addEventListener('click', (e) => {
 
     const { target } = e;
-  
+    
   
     if (!target.matches('button')){
       return;
     }
   
     if (target.classList.contains('operator')) {
-      console.log('operator', target.value);
+      handleOperator(target.value);
+      updateValue();
       return;
     }
   
     if (target.classList.contains('decimal')) {
-      console.log('decimal', target.value);
+      inputDecimal(target.value);
+      updateValue();
       return;
+      
     }
   
     if (target.classList.contains('all-clear')) {
-      console.log('all-clear', target.value);
+      resetCalculator();
+      updateValue();
       return;
     }
 
@@ -81,32 +153,7 @@ for(let i = 0; i < keys.length; i++){
 
 
 
-// let output = document.getElementById("output");
-// let enterNum = 0;
-// let operator = 0;
-// let finalNum;
 
-// let outputVal = output.value;
 
-// function display(num) {
-//   if (output.placeholder === "0") {
-//     output.placeholder = "";
-//   }
-//   output.placeholder += num;
-//   output.value = output.placeholder;
-//   enterNum = Number(output.value);
-//   finalNum = enterNum;
-// }
 
-// function addition() {
-//   display('+')
-//   output.placeholder = "";
-//   operator += enterNum;
-//   output.value = operator;
-//   // console.log(ouput.value)
-// }
 
-// function subtraction() {
-//   operator += -enterNum;
-//   output.value = operator;
-// }
