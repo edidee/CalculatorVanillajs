fa655408e08f5a2f5a3d0122f963e18dd5ad4978
edidee <etokedidionge@gmail.com>
2020-11-10 16:41:44 +0100

    added service worker

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+window.addEventListener("load", () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js").then(registration => {
+      console.log('SM Registration');
+      console.log(registration);
+    }).catch(error => {
+      console.log('SM Registration Failed');
+      console.log(error);
+    });
+  }
+});
+
+
 output = document.getElementById("output")
 
 const calculator = {
---
+++ service-worker.js
@@ -0,0 +1,90 @@
+// installation
+self.addEventListener('install', e => {
+
+    let CACHE_NAME = 'static';
+    let urlsToCache = [
+        '/', 
+        './index.html', 
+        './style.css',
+         './android-chrome-512x512.png',
+         './andriod-chrome-192x192.png'
+    ]
+
+   e.waitUntil(
+       caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)
+       )
+   );
+});
+
+// Activation
+self.addEventListener('activate', e => {
+    let cacheWhitelist = ['products-v2']
+
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map( cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName)
+                    }
+                })
+            )
+        })
+    )
+})
+
+self.addEventListener('activate', (e) => {
+    self.skipWaiting()
+})
+
+
+
+
+
+// fetch
+self.addEventListener('fetch', e => {
+   e.respondWith(
+       caches.open('products-v2').then(cache => {
+           return cache.match(e.request).then(response => {
+               if (response) {
+                   console.log('Cache hit! Fetching response from cache', e.request.url)
+                   return response
+               }
+
+               fetch(e.request).then(response => {
+                   cache.put(e.request, response.clone())
+                   return response
+               })
+           })
+       })
+       
+   );
+}); 
+
+
+// push notification
+if (Notification.permission === 'default') {
+
+    Notification.requestPermission().then(result => {
+        if (result === 'denied') {
+            console.log('Permission denied')
+            return
+        }
+
+        if (result === 'granted') {
+            console.log('Permission granted')
+
+            ServiceWorkerRegistration.pushManager.getSubscription().then (subscription => {
+                if (!subscription) {
+                    const applicationServerKey = ''
+                    ServiceWorkerRegistration.pushManager.subscribe({
+                        userVisibleOnly: true,
+                        applicationServerKey
+                    })
+                } else {
+                    saveSubscriptionInDB(subscription, userId)
+                }
+            })
+        }
+    })
+}
